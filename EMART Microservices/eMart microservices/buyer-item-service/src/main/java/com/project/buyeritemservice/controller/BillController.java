198f5ca1d100ee6949c51830d6b257f6d06e9cfe
ShaikSanaAfreen <shaiksana1198@gmail.com>
2020-03-09 15:59:58 +0530

    first commit

---
+++ EMART Microservices/eMart microservices/buyer-item-service/src/main/java/com/project/buyeritemservice/controller/BillController.java
@@ -0,0 +1,32 @@
+package com.project.buyeritemservice.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.buyeritemservice.pojo.BillPojo;
+import com.project.buyeritemservice.service.BillService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("emart")
+public class BillController {
+	static Logger LOG = Logger.getLogger(BillController.class.getClass());
+
+	@Autowired
+	BillService billService;
+/*Adding bill to the Database*/ 
+	@PostMapping("/bitem")
+
+	BillPojo addBill(@RequestBody BillPojo billPojo) {
+		LOG.info("Entered end point \'emart/bitem \' ");
+		LOG.info("Exited end point \'emart/bitem \' ");
+		return billService.addBill(billPojo);
+	}
+
+}
